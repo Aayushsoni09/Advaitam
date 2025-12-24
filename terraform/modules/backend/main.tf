1689c66593c7e2b4a429bd74867ebffd2f5af459
Aayush Soni <soniaayush24400@gmail.com>
2025-12-24 21:23:36 +0530

    added public ip to ec2

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -71,6 +71,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"             
   key_name               = aws_key_pair.kp.key_name
   subnet_id              = var.subnet_id
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   # Attach the startup script
