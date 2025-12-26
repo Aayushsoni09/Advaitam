4b4560b7a2957ee6d32c3821016770a6e2538d09
Aayush Soni <soniaayush24400@gmail.com>
2025-12-26 21:44:48 +0530

    removed health check url

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -4,10 +4,7 @@ const { createOrder } = require("../controllers/order.controller");
 const router = express.Router();
 
 router.post("/create", createOrder);
-router.get("/api/health", (req, res) => {
-  res.status(200).send("OK") 
-}
-);
+
 
 
 module.exports = router;
